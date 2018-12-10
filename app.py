618709a5bcf4713c39861edb25e8e15de890f036
Deepesh-Kumar <deepeshkumar1991@gmail.com>
2018-12-10 04:05:27 +0000

    Another Change

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from flask_restful import Api
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#app.config['PROPOGATE_EXCEPTIONS'] = True
+app.config['PROPOGATE_EXCEPTIONS'] = True
 app.secret_key = 'deepesh'
 api = Api(app)
 
