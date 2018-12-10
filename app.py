ef67960ae5e773ca94033220b1cf220b67b90f44
Deepesh-Kumar <deepeshkumar1991@gmail.com>
2018-12-10 04:04:15 +0000

    Adding new resource Users

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ from flask_restful import Api
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#app.config['PROPOGATE_EXCEPTIONS'] = True
 app.secret_key = 'deepesh'
 api = Api(app)
 
@@ -36,6 +37,9 @@ api.add_resource(StoreList, '/stores')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
+api.add_resource(User, '/user/<int:user_id>'')
+
+
 
 if __name__ == '__main__':
 
